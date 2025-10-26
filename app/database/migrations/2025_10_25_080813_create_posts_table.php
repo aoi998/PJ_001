c5b902cd1a781b0c931016dec42070dd21cb36eb
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-26 11:45:28 +0900

    idのカラムの定義を変更
    idのカラム定義が誤っており、レコード挿入時にidが自動で採番されないため

--- app/database/migrations/2025_10_25_080813_create_posts_table.php
+++ app/database/migrations/2025_10_25_080813_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->unsignedBigInteger('id')->autoincrement();
+            $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->string('title', 128);
             $table->text('body');
