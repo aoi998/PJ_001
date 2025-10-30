3db0bd49cdeae9fd3b787083350eb6ac15cfab61
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-30 13:53:28 +0900

    投稿一覧のデザインを変更
    画面の幅を簡単に調整できるようにするため

--- app/resources/views/posts/index.blade.php
+++ app/resources/views/posts/index.blade.php
@@ -3,14 +3,16 @@
 @section('title', '投稿一覧')
 
 @section('content')
-<h1 class="text-lg ml-3 mb-3">投稿一覧</h1>
+<div class="container">
+    <h1 class="text-lg ml-3 mb-3">投稿一覧</h1>
 
-<div class="divide-y divide-black mx-3">
-    @foreach ($posts as $post)
-    <div class="gap-2 p-1">
-        <h2>タイトル<br>{{ $post->title }}</h2>
-        <p>投稿内容<br>{{ $post->body }}</p>
+    <div class="divide-y divide-black mx-3">
+        @foreach ($posts as $post)
+        <div class="gap-2 p-1">
+            <h2>タイトル<br>{{ $post->title }}</h2>
+            <p>投稿内容<br>{{ $post->body }}</p>
+        </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
