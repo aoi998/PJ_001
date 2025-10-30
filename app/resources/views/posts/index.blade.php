872c59d37a0271a3606b9293db4cae3c6a3bbf83
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-30 15:19:59 +0900

    投稿一覧のデザインを変更
    投稿内容に画像を表示するため

--- app/resources/views/posts/index.blade.php
+++ app/resources/views/posts/index.blade.php
@@ -3,19 +3,20 @@
 @section('title', '投稿一覧')
 
 @section('content')
-<div class="container mx-3">
+<div class="grid grid-cols-1 gap-4 my-4 mx-3">
     <div class="flex justify-between mx-3 mb-3">
         <h1 class="text-lg">投稿一覧</h1>
         <button type="button" onclick="location.href='/posts/create'" class="bg-blue-400 rounded-md px-3 py-1">新規投稿</button>
     </div>
 
-    <div class="divide-y divide-black mx-3">
-        @foreach ($posts as $post)
-        <div class="gap-2 p-1">
-            <h2>タイトル<br>{{ $post->title }}</h2>
-            <p>投稿内容<br>{{ $post->body }}</p>
+    @foreach ($posts as $post)
+    <div class="overflow-hidden shadow-lg rounded-md h-90 w-60 mx-auto">
+        <img alt="blog photo" src="https://picsum.photos/200" class="max-h-40 w-full object-cover" />
+        <div class="w-full p-4">
+            <p class="text-gray-800 text-xl font-medium mb-2">{{ $post->title }}</p>
+            <p class="text-gray-600 font-light text-md">{{ $post->body }}</p>
         </div>
-        @endforeach
     </div>
+    @endforeach
 </div>
 @endsection
