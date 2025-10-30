5579b29c3f39edec124a078014bba2a98f9f4479
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-30 15:35:37 +0900

    投稿一覧、新規投稿のデザインを変更

--- app/resources/views/posts/create.blade.php
+++ app/resources/views/posts/create.blade.php
@@ -3,23 +3,23 @@
 @section('title', '新規投稿')
 
 @section('content')
-<div class="container mx-auto">
-    <h1 class="text-lg font-bold ml-3 mb-3">新規投稿</h1>
+<div class="grid grid-cols-1 gap-4 my-4 mx-3">
+    <h1 class="font-bold">新規投稿</h1>
 
     <div class="ms-3">
         <form action="{{ route('post.store') }}" method="POST">
             @csrf
             <div class="flex flex-col mb-3">
-                <label for="title" class="text-base">タイトル</label>
-                <input type="text" id="title" name="title" class="rounded-md">
+                <label for="title" class="block text-gray-700 text-sm font-bold mb-2">タイトル</label>
+                <input type="text" id="title" name="title" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
             </div>
             <div class="flex flex-col mb-3">
-                <label for="body" class="text-base">本文</label>
-                <textarea id="body" name="body" class="rounded-md"></textarea>
+                <label for="body" class="block text-gray-700 text-sm font-bold mb-2">本文</label>
+                <textarea id="body" name="body" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outlin"></textarea>
             </div>
             <div class="flex justify-center gap-3 mb-3">
-                <button type="button" onclick="location.href='/posts'" class="bg-gray-300 rounded-md px-3 py-1">戻る</button>
-                <button type="submit" class="bg-blue-300 rounded-md px-3 py-1">投稿</button>
+                <button type="button" onclick="location.href='/posts'" class="bg-gray-500 text-white rounded-md px-4 py-2">戻る</button>
+                <button type="submit" class="bg-blue-500 text-white rounded-md px-4 py-2">投稿</button>
             </div>
         </form>
     </div>
--- app/resources/views/posts/index.blade.php
+++ app/resources/views/posts/index.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
 <div class="grid grid-cols-1 gap-4 my-4 mx-3">
-    <div class="flex justify-between mx-3 mb-3">
-        <h1 class="text-lg">投稿一覧</h1>
-        <button type="button" onclick="location.href='/posts/create'" class="bg-blue-400 rounded-md px-3 py-1">新規投稿</button>
+    <div class="flex justify-between items-center mx-3 mb-3">
+        <h1 class="font-bold">投稿一覧</h1>
+        <button type="button" onclick="location.href='/posts/create'" class="bg-blue-500 text-white rounded-md px-4 py-2">新規投稿</button>
     </div>
 
     @foreach ($posts as $post)
