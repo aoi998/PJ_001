e699d9e67d6b86c6b59d0372ea5f38d2f83eebda
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-30 15:01:37 +0900

    新規投稿画面に遷移するボタンを作成

--- app/resources/views/posts/index.blade.php
+++ app/resources/views/posts/index.blade.php
@@ -3,8 +3,11 @@
 @section('title', '投稿一覧')
 
 @section('content')
-<div class="container">
-    <h1 class="text-lg ml-3 mb-3">投稿一覧</h1>
+<div class="container mx-3">
+    <div class="flex justify-between mx-3 mb-3">
+        <h1 class="text-lg">投稿一覧</h1>
+        <button type="button" onclick="location.href='/posts/create'" class="bg-blue-400 rounded-md px-3 py-1">新規投稿</button>
+    </div>
 
     <div class="divide-y divide-black mx-3">
         @foreach ($posts as $post)
