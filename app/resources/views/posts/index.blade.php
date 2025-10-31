141055769e089a6837bc7832f3c4267557826df3
aoi998 <aoi.sakamoto998@gmail.com>
2025-10-31 16:29:22 +0900

    投稿一覧にダークモードのデザインを追加

--- app/resources/views/posts/index.blade.php
+++ app/resources/views/posts/index.blade.php
@@ -3,19 +3,19 @@
 @section('title', '投稿一覧')
 
 @section('content')
-<div class="grid grid-cols-1 gap-4 my-4 mx-3">
-    <div class="flex justify-between items-center mx-3 mb-3">
-        <h1 class="font-bold">投稿一覧</h1>
-        <button type="button" onclick="location.href='/post/create'" class="bg-blue-500 text-white rounded-md px-4 py-2">新規投稿</button>
+<div class="grid grid-cols-1 gap-4 pt-3 pb-3 mx-3">
+    <div class="flex justify-between items-center mx-3">
+        <h1 class="text-lg font-bold dark:text-white">投稿一覧</h1>
+        <button type="button" onclick="location.href='/post/create'" class="bg-blue-500 dark:bg-gray-200 dark:text-gray-800 text-white rounded-md px-4 py-2">新規投稿</button>
     </div>
 
     @foreach ($posts as $post)
-    <div class="overflow-hidden shadow-lg rounded-md h-90 w-60 mx-auto">
+    <div class="overflow-hidden shadow-lg rounded-md h-90 w-60 mx-auto bg-gray-300 dark:bg-gray-800">
         <a href="{{ route('post.show', ['post' => $post]) }}" class="block h-full w-full">
             <img alt="blog photo" src="https://picsum.photos/200" class="max-h-40 w-full object-cover" />
             <div class="w-full p-4">
-                <p class="text-gray-800 text-xl font-medium mb-2">{{ $post->title }}</p>
-                <p class="text-gray-600 font-light text-md">{{ $post->body }}</p>
+                <p class="text-gray-800 dark:text-gray-200 text-xl font-medium mb-2">{{ $post->title }}</p>
+                <p class="text-gray-600 dark:text-gray-400 font-light text-md">{{ $post->body }}</p>
             </div>
         </a>
     </div>
